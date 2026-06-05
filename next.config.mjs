0296d912ceface081ecf5982e8d923cf0cbb3827
kothiyalritesh <riteshkothiyal2002@gmail.com>
2026-06-05 17:46:44 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio",
   eslint: {
     ignoreDuringBuilds: true,
   },
